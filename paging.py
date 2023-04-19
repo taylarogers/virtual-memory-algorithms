8c95813806cd12ad3024c85a914bf9af7edd1c4e
taylarogers <110628845+taylarogers@users.noreply.github.com>
2023-04-19 14:34:36 +0200

    Optimisation of Algorithms

--- paging.py
+++ paging.py
@@ -2,90 +2,108 @@ import sys
 from random import seed
 from random import randint
 
+# The oldest page in memory is removed, and new page can be inserted
 def FIFO(size, pages):
     frames = []
     pageFaults = 0
     
     for page in pages:
+        # Check if page is in memory
         if frames.count(page) == 0:
             pageFaults += 1
 
+            # Check if all frames in memory are in use
+            if len(frames) < size:
+                frames.append(page)
+            else:
+                # Remove first frame (oldest)
+                frames.pop(0)
+                frames.append(page)
+        else:
+            # Page can just be accessed an nothing further to be done
+            continue
+
+    return(pageFaults)
+
+# The page that was least recently used in memory is removed, and a new page can be inserted
+def LRU(size, pages):
+    frames = []
+    pageFaults = 0
+
+    for page in pages:
+        # Check if frame is in memory
+        if frames.count(page) == 0:
+            pageFaults += 1
+
+            # Check if frame 
             if len(frames) < size:
                 frames.append(page)
             else:
                 frames.pop(0)
                 frames.append(page)
         else:
-            continue
-
-    return(pageFaults)
-
-def LRU(size, pages):
-    frames = []
-    pageFaults = 0
-    usedTime = []
-
-    for page in pages:
-        if frames.count(page) == 0:
-            pageFaults += 1
-
-            for i in range(len(frames)):
-                usedTime[i] += 1
-
-            if len(frames) < size:
-                frames.append(page)
-                usedTime.append(0)
-            else:
-                frames.pop(usedTime.index(max(usedTime)))
-                usedTime.pop(usedTime.index(max(usedTime)))                
-                
-                frames.append(page)
-                usedTime.append(0)
-        else:
-            for i in range(len(frames)):
-                usedTime[i] += 1
-
-            usedTime[frames.index(page)] = 0
+            index = frames.index(page)
+            frames.append(frames[index])
+            frames.pop(index)
         
     return(pageFaults)
-
+# The page the will be used the latest in the future is removed, and new page added
 def OPT(size, pages):
     frames = []
     pageFaults = 0
 
+    # Run through copy of array - allow you to pop values off the array of future values and just search through those
     for page in pages[:]:
+        # Check if frame is in memory
         if frames.count(page) == 0:
             pageFaults += 1
+            # Remove page to keep track of future values
             pages.pop(0)
 
+            # Check if there is space in memory or not
             if len(frames) < size:
                 frames.append(page)
             else:
+                # Keep range of the indexes of the next occurring values to be able to find the largest
                 nextOccurances = []
 
+                # Find the indexes of the next occurances for each value in memory
                 for i in range(size):
                     if pages.count(frames[i]) != 0:
                         nextOccurances.append(pages.index(frames[i]))
                     else:
-                        nextOccurances.append(10000000000)
+                        # Use this value when the page is going to be used later
+                        nextOccurances.append(-1)
                 
-                frames.pop(nextOccurances.index(max(nextOccurances)))
+                # Remove the value in frames that has the largest index valueor isn't going to be mentioned later
+                if nextOccurances.count(-1) == 0:
+                    frames.pop(nextOccurances.index(max(nextOccurances)))
+                else:
+                    frames.pop(nextOccurances.index(-1))
+
                 frames.append(page)
         else:
+            # Remove page to keep track of future values
             pages.pop(0)
             continue
     
     return(pageFaults)
 
 def main():
-    numPages = 20
+    # Hardcoded value of many pages should be used to test the algorithms
+    numPages = 7
     pages = []
 
+    # Create random values between 0 and 9 that will be used
     for i in range(numPages):
         pages.append(randint(0,9))
 
+    pages = [7,0,1,2,0,3,0,4,2,3,0,3,2,1,2,0,1,7,0,1]
+
+    # Retrieve terminal command from user stating number of frames in memory
     size = int(sys.argv[1])
 
+    # Determine page faults for each algorithmn and print to screen
     print(pages)
     print('FIFO:', FIFO(size, pages), 'page faults.')
     print('LRU:', LRU(size, pages), 'page faults.')
